b3bebc8d2d7576a611032e2e62755002a75ef3ec
Amirho3eindev <Amirho3eindev@gmail.com>
2020-03-31 19:28:41 +0430

    Initial Commit

---
+++ RoseChat.Api/DataLayer/Context/RoseChatContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoseChat.Api.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoseChat.Api.DataLayer.Context
+{
+    public class RoseChatContext:DbContext
+    {
+      
+        public RoseChatContext(DbContextOptions<RoseChatContext>options):base(options)
+        {
+        
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
